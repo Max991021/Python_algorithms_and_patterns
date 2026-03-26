cbf1c01bd6eba058c8099d8784243381f60e2555
Max991021 <leshopep@gmail.com>
2026-03-26 23:36:52 +0200

    double loops or more depending on the shape, next is number shapes

--- patterns.py
+++ patterns.py
@@ -22,6 +22,7 @@ for i in range(10,0,-1):
     for j in range(i):
         print('*', end=" ")
     print()
+print('___________________________')
 '''
 ***
 **
@@ -41,18 +42,23 @@ for i in range(10,0,-1):
     for z in range(i):
         print('*',end=" ")
     print()
+print('___________________________')
 '''
   *
  ***
 *****
 '''
 
-# n = 3
-# for i in range(n+1):
-#     spaces = '  ' * (n - i)
-#     stars = '* ' * i
-#     print( stars+spaces)
-
+n = 5
+for i in range(n):
+    for j in range(i,n):
+        print(' ',end=" ")
+    for j in range(i):
+        print('*',end=" ")
+    for j in range(i+1):
+        print("*", end=' ')
+    print()
+print('___________________________')
 n = 5
 
 for i in range(n):
@@ -62,9 +68,70 @@ for i in range(n):
         print()
     else:
         print("* "+" "*6+"*")
-
+print('___________________________')
 
 for i in range(n):
     for j in range(n-i):
         print("*", end=' ')
+    print()
+print('___________________________')    
+'''
+
+     *
+    ***
+   *****
+'''
+
+for i in range(n):
+    for j in range(i,n):
+        print(' ',end=" ")
+    for j in range(i+1):
+        print('*',end=" ")
+    for j in range(i):
+        print('*',end=" ")
+    print()
+print('___________________________')   
+
+'''
+*******
+ *****
+  ***
+   *
+'''
+
+for i in range(n):
+    for j in range(i+1):
+        print(' ',end=" ")
+    for j in range(i,n):
+        print('*',end=' ')
+    for i in range(i,n-1):
+        print('*',end=' ')
+    print()
+print('___________________________')
+
+'''
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+'''
+
+for i in range(n-1):
+    for j in range(i,n):
+        print(" ",end=" ")
+    for j in range(i):
+        print('*',end=" ")
+    for j in range(i+1):
+        print("*", end=" ")
+    print()
+for i in range(n):
+    for j in range(i+1):
+        print(' ',end=" ")
+    for j in range(i,n):
+        print('*',end=' ')
+    for j in range(i,n-1):
+        print('*',end=' ')
     print()
