fa4689078920273e5e8fd037ce1799204730bdc7
Max991021 <leshopep@gmail.com>
2026-03-27 20:30:27 +0200

    Need to fix double hill

--- patterns.py
+++ patterns.py
@@ -135,3 +135,25 @@ for i in range(n):
     for j in range(i,n-1):
         print('*',end=' ')
     print()
+print('___________________________')
+
+'''
+double hill
+'''
+
+for i in range(n-1):
+    for j in range(i,n):
+        print(" ",end=" ")
+    for j in range(i):
+        print('*',end=" ")
+    for j in range(i+1):
+        print("*", end=" ")
+    print()
+for i in range(n-1):
+    for j in range(i,n):
+        print(" ",end=" ")
+    for j in range(i):
+        print('*',end=" ")
+    for j in range(i+1):
+        print("*", end=" ")
+    print()
