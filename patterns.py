5237e6489c5b87b8cd3f6707d96b6bf08fae0ee1
Max991021 <leshopep@gmail.com>
2026-04-12 21:42:10 +0200

    Testing

--- patterns.py
+++ patterns.py
@@ -185,28 +185,48 @@ for i in range(n-1):
 '''
 
 n = 5
-for i in range(1, n + 1):
-    # Create the stars with a space after each one
-    row_stars = "* " * i
-    
-    # Strip the very last trailing space so it centers perfectly
-    row_stars = row_stars.strip()
-    
-    # Center it based on the width of the bottom row
-    print(row_stars.center(n * 2))
-for i in range(n-1,0,-1):
-    # Create the stars with a space after each one
-    row_stars = "* " * i
-    
-    # Strip the very last trailing space so it centers perfectly
-    row_stars = row_stars.strip()
-    
-    # Center it based on the width of the bottom row
-    print(row_stars.center(n * 2))
-# for row in range(n):
-#     results = ''
-#     for col in range(row+1):
-#         results += '*'
-#     if row < n-1:
-#         results += '\n'
-#     print(results.center(n))
+def dia(n):
+    hi = []
+    ho = []
+    for i in range(1, n + 1):
+        # Create the stars with a space after each one
+        row_stars = "* " * i
+        
+        # Strip the very last trailing space so it centers perfectly
+        row_stars = row_stars.strip()
+        
+        # Center it based on the width of the bottom row
+        hi.append(row_stars.center(n * 2))
+    print(hi)
+    for i in range(n-1,0,-1):
+        # Create the stars with a space after each one
+        row_stars = "* " * i
+        
+        # Strip the very last trailing space so it centers perfectly
+        row_stars = row_stars.strip()
+        
+        # Center it based on the width of the bottom row
+        ho.append(row_stars.center(n * 2))
+    j = '\n'.join(ho)
+    return '\n'.join(hi)+'\n'+j
+print(dia(5))
+def right(n):
+    results = ''
+    for row in range(n):
+        for col in range(n - row):
+            results += '*'
+        if row < n-1:
+            results += '\n'
+    return results
+print(right(5))
+
+
+def hallow(n):
+    ho = []
+    for row in range(1,n+1):
+        for col in range(1,2*n):
+            if col == n-row+1 or col== n+row -1 or row==n:
+                ho.append('* '*i)
+            else:
+                ho.append(''*i)
+        
